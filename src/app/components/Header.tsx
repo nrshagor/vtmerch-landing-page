6780b9ab15b4c371e9f87ab407f46f2925870c68
nrshagor <noorerabbishagor@gmail.com>
2024-10-05 00:19:15 +0600

    hero section product design

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,30 +3,28 @@ import styles from "@/app/style/Header.module.css";
 import Image from "next/image";
 const Header = () => {
   return (
-    <div>
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <div className={styles.image}>
-            <Image
-              src="/ValuetainmentWhite.svg"
-              alt="VTMerch Logo"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className={styles.text}>
-            <h3>VTMERCH</h3>
-          </div>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <div className={styles.image}>
+          <Image
+            src="/ValuetainmentWhite.svg"
+            alt="VTMerch Logo"
+            width={100}
+            height={100}
+          />
         </div>
-        <nav className={styles.nav}>
-          <button className={styles.menuButton}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </nav>
-      </header>
-    </div>
+        <div className={styles.text}>
+          <h3>VTMERCH</h3>
+        </div>
+      </div>
+      <nav className={styles.nav}>
+        <button className={styles.menuButton}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </nav>
+    </header>
   );
 };
 
--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -2,68 +2,65 @@ import React from "react";
 import styles from "@/app/style/HeroSection.module.css";
 const HeroSection = () => {
   return (
-    <div>
-      <section className={styles.heroSection}>
-        <div className={styles.heroText}>
-          <h1>Future Looks Bright!</h1>
-        </div>
-        <div className={styles.productGrid}>
-          <div className={styles.product}>
-            <div className={styles.imageWrapper}>
-              <img src="/img/img9.png" alt="Product 1" />
-            </div>
+    <section className={styles.heroSection}>
+      <div className={styles.heroText}>
+        <h1>Future Looks Bright!</h1>
+      </div>
+      <div className={styles.productGrid}>
+        <div className={styles.product}>
+          <div className={styles.imageWrapper}>
+            <img src="/img/img9.png" alt="Product 1" />
+          </div>
 
-            <p>Men&apos;s Black Crewneck</p>
-            <div className={styles.prices}>
-              <span>$34.99</span>
-              <span>$34.99</span>
-            </div>
-          </div>
-          <div className={styles.product}>
-            <div className={styles.imageWrapper}>
-              <img src="/img/img10.png" alt="Product 2" />
-            </div>
-
-            <p>Men&apos;s Black Hoodie</p>
-            <div className={styles.prices}>
-              <span>$34.99</span>
-              <span>$34.99</span>
-            </div>
-          </div>
-          <div className={styles.product}>
-            <div className={styles.imageWrapper}>
-              {" "}
-              <img src="/img/img11.jpeg" alt="Product 1" />
-            </div>{" "}
-            <p>Men&apos;s Black Crewneck</p>
-            <div className={styles.prices}>
-              <span>$34.99</span>
-              <span>$34.99</span>
-            </div>
-          </div>
-          <div className={styles.product}>
-            <div className={styles.imageWrapper}>
-              <img src="/img/img12.png" alt="Product 2" />
-            </div>{" "}
-            <p>Men&apos;s Black Hoodie</p>
-            <div className={styles.prices}>
-              <span>$34.99</span>
-              <span>$34.99</span>
-            </div>
-          </div>
-          <div className={styles.product}>
-            <div className={styles.imageWrapper}>
-              <img src="/img/img11.jpeg" alt="Product 1" />
-            </div>{" "}
-            <p>Men&apos;s Black Crewneck</p>
-            <div className={styles.prices}>
-              <span>$34.99</span>
-              <span>$34.99</span>
-            </div>
+          <p>Men&apos;s Black Crewneck</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
           </div>
         </div>
-      </section>
-    </div>
+        <div className={styles.product}>
+          <div className={styles.imageWrapper}>
+            <img src="/img/img10.png" alt="Product 2" />
+          </div>
+
+          <p>Men&apos;s Black Hoodie</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
+        </div>
+        <div className={styles.product}>
+          <div className={styles.imageWrapper}>
+            <img src="/img/img11.jpeg" alt="Product 1" />
+          </div>
+          <p>Men&apos;s Black Crewneck</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
+        </div>
+        <div className={styles.product}>
+          <div className={styles.imageWrapper}>
+            <img src="/img/img12.png" alt="Product 2" />
+          </div>
+          <p>Men&apos;s Black Hoodie</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
+        </div>
+        <div className={styles.product}>
+          <div className={styles.imageWrapper}>
+            <img src="/img/img11.jpeg" alt="Product 1" />
+          </div>
+          <p>Men&apos;s Black Crewneck</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
