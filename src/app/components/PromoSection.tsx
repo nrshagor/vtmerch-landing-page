4461eb711f1aa252c9b7fe9dbbcbfe22dc4dff30
nrshagor <noorerabbishagor@gmail.com>
2024-10-05 02:34:21 +0600

    promo section design

--- src/app/components/PromoSection.tsx
+++ src/app/components/PromoSection.tsx
@@ -3,17 +3,45 @@ import styles from "@/app/style/PromoSection.module.css";
 const PromoSection = () => {
   return (
     <section className={styles.promoSection}>
-      <h2>Buy 1 Get 1 Free</h2>
+      <div className={styles.title}>
+        <h2>Buy 1 get 1 Free</h2>
+        <h2>
+          on <span>best selling products</span>
+        </h2>
+      </div>
+
       <div className={styles.productGrid}>
         <div className={styles.product}>
-          <img src="/hat1.jpg" alt="White Cap" />
-          <p>White Cap</p>
-          <span>$12.99</span>
+          <img src="/img/hat1.jpeg" alt="White Cap" />
+          <p>Army Green Active Short Sleeve</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
         </div>
         <div className={styles.product}>
-          <img src="/hat2.jpg" alt="Black Beanie" />
-          <p>Black Beanie</p>
-          <span>$12.99</span>
+          <img src="/img/hat2.jpeg" alt="Black Beanie" />
+          <p>Army Green Active Short Sleeve</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
+        </div>
+        <div className={styles.product}>
+          <img src="/img/hat3.jpeg" alt="Black Beanie" />
+          <p>Army Green Active Short Sleeve</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
+        </div>
+        <div className={styles.product}>
+          <img src="/img/hat4.jpeg" alt="Black Beanie" />
+          <p>Army Green Active Short Sleeve</p>
+          <div className={styles.prices}>
+            <span>$34.99</span>
+            <span>$34.99</span>
+          </div>
         </div>
         {/* Repeat for other products */}
       </div>
