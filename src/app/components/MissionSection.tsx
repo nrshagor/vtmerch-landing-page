20c0bb52195aeee34110bb51f9d3acf4bca9c101
nrshagor <noorerabbishagor@gmail.com>
2024-10-05 15:38:09 +0600

    mission section design

--- src/app/components/MissionSection.tsx
+++ src/app/components/MissionSection.tsx
@@ -4,13 +4,19 @@ import styles from "@/app/style/MissionSection.module.css";
 const MissionSection = () => {
   return (
     <section className={styles.missionSection}>
-      <h2>Our Mission</h2>
-      <p>
-        Our mission is to enlighten, entertain, and empower current and future
-        leaders around the world.
-      </p>
-      <div className={styles.image}>
-        <img src="/patrick-bet-david.jpg" alt="Patrick Bet-David" />
+      <div className={styles.missionLeftSection}>
+        <div className={styles.icon}>
+          <img src="/icon/quotes.svg" alt="Patrick Bet-David" />
+        </div>
+        <p>
+          Our mission is to enlighten, entertain and empower current and future
+          leaders around the world.
+        </p>
+      </div>
+      <div className={styles.missionRightSection}>
+        <div className={styles.image}>
+          <img src="/img/mission.png" alt="Patrick Bet-David" />
+        </div>
       </div>
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,10 @@ export default function Home() {
     <div className={styles.page}>
       <HeroSection />
       <PromoSection />
-      <SignedCollection />
-      <MissionSection />
+      <div className="groupSection">
+        <SignedCollection />
+        <MissionSection />
+      </div>
     </div>
   );
 }
